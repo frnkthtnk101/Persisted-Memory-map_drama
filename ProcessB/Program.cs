969d8c8bca5a804144a19a5698dcdb89c5e9733b
Franco Pettigrosso <fpettigrosso@holyfamily.edu>
2019-05-15 09:29:51 -0400

    added project files and directories

---
+++ ProcessB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProcessB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
